bea651a1052ea48523849634ec98c170eb17e17d
Williamgabrielsilv <140897830+Williamgabrielsilv@users.noreply.github.com>
2025-09-19 14:10:00 -0300

    Update IUserRepository.cs

--- Projeto_ControleDeFuncionários/Repositories/Interfaces/IUserRepository.cs
+++ Projeto_ControleDeFuncionários/Repositories/Interfaces/IUserRepository.cs
@@ -6,5 +6,20 @@ namespace Projeto_ControleDeFuncionários.Repositories.Interfaces
     {
         Task<User?>GetByEmailAsync(string email);
         Task<User> CreateAsync(User user);
+    
+    /*CREATE
+        Task<User> CreateAsync(User user);
+
+        // READ
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByEmailAsync(string email);
+        Task<IEnumerable<User>> GetAllAsync();
+
+        // UPDATE
+        Task UpdateAsync(User user);
+
+        // DELETE
+        Task DeleteAsync(int id);*/
     }
+    
 }
