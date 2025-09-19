cebfc23a34b1b1a19128445b40340fb0709bbb11
Williamgabrielsilv <140897830+Williamgabrielsilv@users.noreply.github.com>
2025-09-19 14:15:54 -0300

    Update UserRepository.cs

--- Projeto_ControleDeFuncionários/Repositories/UserRepository.cs
+++ Projeto_ControleDeFuncionários/Repositories/UserRepository.cs
@@ -21,5 +21,42 @@ namespace Projeto_ControleDeFuncionários.Repositories
         {
             return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
         }
+    
+
+        /* READ
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // UPDATE
+        public async Task UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        // DELETE
+        public async Task DeleteAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }*/
     }
+
+
 }
